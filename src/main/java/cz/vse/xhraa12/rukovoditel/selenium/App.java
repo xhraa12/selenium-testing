5e60f473abea9b73808822a5a852c4a9aa6b7404
Anna <hrabalova.anna@gmail.com>
2019-11-30 19:19:19 +0100

    zprovozni ze cviceni

---
+++ src/main/java/cz/vse/xhraa12/rukovoditel/selenium/App.java
@@ -0,0 +1,12 @@
+package cz.vse.xhraa12.rukovoditel.selenium;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World!");
+    }
+}
