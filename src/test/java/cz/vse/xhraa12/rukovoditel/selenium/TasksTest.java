9840430ce772341d8009955220d85ebf8d8e778a
Anna <hrabalova.anna@gmail.com>
2019-12-28 16:47:19 +0100

    TypesOfTasks

--- src/test/java/cz/vse/xhraa12/rukovoditel/selenium/TasksTest.java
+++ src/test/java/cz/vse/xhraa12/rukovoditel/selenium/TasksTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,9 +15,26 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+class Task {
+    public String type;
+    public String name;
+    public String status;
+    public String priority;
+    public String description;
+
+    public Task(String type, String name, String status, String priority, String description) {
+        this.type = type;
+        this.name = name;
+        this.status = status;
+        this.priority = priority;
+        this.description = description;
+    }
+}
+
 public class TasksTest {
     private ChromeDriver driver;
     private LoginTest auth;
+    public final String projectName = "xhraa12";
 
     @Before
     public void init() {
@@ -38,25 +56,62 @@ public class TasksTest {
         driver.close();
     }
 
+    public void navigateToMyProjectPage() {
+        // Navigate to Projects list
+        By menuProjects = By.cssSelector("body > div.page-container > div.page-sidebar-wrapper.noprint > div > div > ul > li:nth-child(4) > a > i");
+        Util.waitFor(driver, menuProjects)
+            .click();
+
+        // Find my project and click through on to detail
+        String projectName = this.projectName;
+        // Wait for title
+
+        Util.waitFor(driver, By.cssSelector("body > div.page-container > div.page-content-wrapper > div > div > div.row > div > div:nth-child(2) > div > h3"));
+        // Check if for filter reset
+        By resetFilterSelector = By.cssSelector(".reset_search");
+        boolean projectFilterApplied = Util.waitFor(driver, By.cssSelector("body"))
+            .findElements(resetFilterSelector)
+            .size() > 0;
+        if (projectFilterApplied) {
+            Util.waitFor(driver, resetFilterSelector)
+                .click();
+        }
+        Util.waitFor(driver, By.xpath(String.format("//*[text()=\"%s\"]", projectName)))
+            .click();
+    }
+    public void createTask(Task task) {
+        Util.waitFor(driver, By.cssSelector("#fields_168"))
+            .sendKeys(task.name);
+        new Select(Util.waitFor(driver, By.cssSelector("#fields_167")))
+            .selectByVisibleText(task.type);
+        new Select(Util.waitFor(driver, By.cssSelector("#fields_169")))
+            .selectByVisibleText(task.status);
+        new Select(Util.waitFor(driver, By.cssSelector("#fields_170")))
+            .selectByVisibleText(task.priority);
+        // To edit description, we need to switch to iframe and back
+        driver.switchTo().frame(driver.findElement(By.cssSelector("#cke_1_contents > iframe")));
+        driver.findElement(By.cssSelector("body"))
+            .sendKeys(task.description);
+        driver.switchTo().defaultContent();
+        // Submit the form
+        Util.waitFor(driver, By.cssSelector("#items_form > div.modal-footer > button.btn.btn-primary.btn-primary-modal-action"))
+            .click();
+    }
+    public void openCreateTaskDialogue() {
+        // Click Add task
+        Util.waitFor(driver, By.cssSelector("button.btn.btn-primary"))
+            .click();
+    }
+
     @Test
     public void deleteTaskTest() {
         // given
         driver.manage().window().maximize();
         auth.login(auth.username, auth.password);
         // when
-        // Navigate to Projects list
-        By menuProjects = By.cssSelector("body > div.page-container > div.page-sidebar-wrapper.noprint > div > div > ul > li:nth-child(4) > a > i");
-        Util.waitFor(driver, menuProjects)
-            .click();
-        // Find my project and click through on to detail
-        String projectName = "xhraa12";
+        this.navigateToMyProjectPage();
         String taskName = "xhraa12";
-        String selector = String.format("//*[text()=\"%s\"]", projectName);
-        Util.waitFor(driver, By.xpath(selector))
-            .click();
-        // Click Add task
-        Util.waitFor(driver, By.cssSelector("body > div.page-container > div.page-content-wrapper > div > div > div.row > div > div:nth-child(7) > div:nth-child(1) > div > button"))
-            .click();
+        this.openCreateTaskDialogue();
         // Fil in the form
         String taskType = "Task";
         String taskStatus = "New";
@@ -117,5 +172,83 @@ public class TasksTest {
                 .getText(),
             "Tasks"
         );
+    }
+
+    public void resetTasks() {
+        // Clear filters
+        // If there is a thrashcan in Filters box
+        List<WebElement> thrashcans = Util.waitFor(driver, By.cssSelector(".portlet.portlet-filters-preview.noprint"))
+                .findElements(By.cssSelector(".fa.fa-trash-o"));
+        if (thrashcans.size() > 0) {
+            // Click it to remove all filters
+            thrashcans.get(0).click();
+        }
+        // Delete all items
+        // Check all items
+        Util.waitFor(driver, By.cssSelector(".select_all_items"))
+            .click();
+        // Open With Selected dropdown
+        Util.waitFor(driver, By.cssSelector(".btn.btn-default.dropdown-toggle"))
+            .click();
+        // Click Delete
+        Util.waitFor(driver, By.cssSelector(".entitly-listing-buttons-left .fa.fa-trash-o"))
+            .click();
+        // Confirm
+        Util.waitFor(driver, By.cssSelector(".btn.btn-primary.btn-primary-modal-action"))
+            .click();
+    }
+    @Test
+    public void typeTasksTest() {
+        // Given
+        driver.manage().window().maximize();
+        auth.login(auth.username, auth.password);
+        Task[] data = {
+                new Task("Task", "Task 1", "New", "Medium", ""),
+                new Task("Task", "Task 2", "Open", "Medium", ""),
+                new Task("Task", "Task 3", "Waiting", "Medium", ""),
+                new Task("Task", "Task 4", "Done", "Medium", ""),
+                new Task("Task", "Task 5", "Closed", "Medium", ""),
+                new Task("Task", "Task 6", "Paid", "Medium", ""),
+                new Task("Task", "Task 7", "Canceled", "Medium", ""),
+
+        };
+        this.navigateToMyProjectPage();
+        this.resetTasks();
+        By tasksItemsSelector = By.cssSelector(".item_heading_link");
+        // When
+        // Create each task from data list
+        for (Task task: data) {
+            this.openCreateTaskDialogue();
+            this.createTask(task);
+            // We are back on the list
+            Util.waitFor(driver, By.cssSelector("body > div.page-container > div.page-content-wrapper > div > div > div.row > div > div:nth-child(7) > div:nth-child(1) > div > button"));
+        }
+        // Apply default filters
+        // Open Filter dropdown
+        Util.waitFor(driver, By.cssSelector("body > div.page-container > div.page-content-wrapper > div > div > div.row > div > div.portlet.portlet-filters-preview.noprint > div.portlet-title > div.caption > div:nth-child(1) > button"))
+            .click();
+        // Select Default Filters
+        Util.waitFor(driver, By.cssSelector("body > div.page-container > div.page-content-wrapper > div > div > div.row > div > div.portlet.portlet-filters-preview.noprint > div.portlet-title > div.caption > div:nth-child(1) > ul > li:nth-child(1) > a"))
+            .click();
+        // Then
+        // Only three tasks are shown in the list
+        Assert.assertEquals(
+            Util.waitFor(driver, tasksItemsSelector)
+                .findElements(By.cssSelector("*"))
+                .size(),
+            3
+        );
+
+
+        // Then
+
+        // Each created task is found in the tasks list
+//        for (Task task: data) {
+//            Assert.assertEquals(
+//                Util.waitFor(driver, By.cssSelector(String.format("//*[@class=\"item_heading_link\" and text()=\"%s\"]", task.name)))
+//                    .getText(),
+//                task.name
+//            );
+//        }
     }
 }
